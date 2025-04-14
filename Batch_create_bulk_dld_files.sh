7d0e35fc339fb03f5a6e78cca703a1970e4d8863
paproki <anthony.paproki@gmail.com>
2025-04-15 09:52:08 +1000

    Added batch bulk file create script

--- Batch_create_bulk_dld_files.sh
+++ Batch_create_bulk_dld_files.sh
@@ -4,13 +4,23 @@ all_field_config="${HOME}/Dev/UKBUtils/ukb_fields_new.txt"
 
 while read BulkID;
 do
-    echo " > > > $BulkID < < <"
-    fields="participant.eid"
-    
-    for bField in `cat $all_field_config | grep p$BulkID`;
-    do
-        fields="${fields},${bField}"
-    done
-    echo "dx extract_dataset record-GzfyQXjJ6yvFxGkYpqVK9G9G --fields $fields --output participants_${BulkID}.csv"
-    echo "bash ukb-rap/Filter_dx_output.sh --input participants_${BulkID}.csv --output ${BulkID}.bulk"
+    if [[ ! -f participants_${BulkID}.csv ]] || [[ ! -f Configs/${BulkID}.bulk ]];then
+
+        echo " > > > > > > > > > > > > > > > > > > > > >  $BulkID < < < < < < < < < < < < < < < < < < < < <"
+        fields="participant.eid"
+        
+        for bField in `cat $all_field_config | grep p$BulkID`;
+        do
+            fields="${fields},${bField}"
+        done
+        while [[ ! -f Configs/${BulkID}.bulk ]];
+        do
+            echo "dx extract_dataset record-GzfyQXjJ6yvFxGkYpqVK9G9G --fields $fields --output participants_${BulkID}.csv"
+            dx extract_dataset record-GzfyQXjJ6yvFxGkYpqVK9G9G --fields $fields --output participants_${BulkID}.csv
+            echo "bash ukb-rap/Filter_dx_output.sh --input participants_${BulkID}.csv --output Configs/${BulkID}.bulk"
+            bash ukb-rap/Filter_dx_output.sh --input participants_${BulkID}.csv --output Configs/${BulkID}.bulk
+        done
+
+        echo " > > > > > > > > > > > > > > > > > > > > > > > < < < < < < < < < < < < < < < < < < < < < < <"
+    fi
 done < All_bulk_ids.txt
